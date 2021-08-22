148f08bbbcdba5947d8c9d9c41d4c22f70e58fd7
Xiaofei Ge <xfge.cn@gmail.com>
2021-08-22 15:19:56 +0800

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by 葛笑非 on 2021/8/22.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
